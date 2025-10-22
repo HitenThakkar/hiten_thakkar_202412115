3f5a156ccddde5f724923d0fad9a46d61288ee1d
HitenThakkar <hitenthakkar512@gmail.com>
2025-10-23 01:47:42 +0530

    MY-SQL in env uri change

--- backend/prisma/migrations/20251022200655_init/migration.sql
+++ backend/prisma/migrations/20251022200655_init/migration.sql
@@ -1,5 +1,5 @@
 -- DropForeignKey
-ALTER TABLE `orderitem` DROP FOREIGN KEY `OrderItem_orderId_fkey`;
+ALTER TABLE `OrderItem` DROP FOREIGN KEY `OrderItem_orderId_fkey`;
 
 -- AddForeignKey
 ALTER TABLE `OrderItem` ADD CONSTRAINT `OrderItem_orderId_fkey` FOREIGN KEY (`orderId`) REFERENCES `Orders`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
